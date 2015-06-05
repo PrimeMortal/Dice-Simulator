558943b28f506be1cd37aa02c3c6fbe8579d370c
PrimeMortal <ordinary.acct@gmail.com>
2015-06-05 20:56:50 +0800

    used a while loop to allow rerunning the program

--- Dice-Simulator.py
+++ Dice-Simulator.py
@@ -1,6 +1,12 @@
 import random
 
-# generate random integer from 1-6
-output = random.randint(1, 6)
-# print output
-print(output)
+run = "y"
+
+while run != "n":
+    # generate random integer from 1-6
+    output = random.randint(1, 6)
+    # print output
+    print(output)
+    run = input("Run again? y/n   ")
+else:
+    print("Thank you for using PrimeMortal's dice simulator.")
